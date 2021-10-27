947f525346c55ca41c4addd5dfb9e9fa8c1450ea
hyunlee214 <hyun110214@naver.com>
2021-10-27 14:32:09 +0900

    [테스트서버]: 구조설정

---
+++ app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = 3000;
+
+const home = require("./routes/home");
+const usersRouter = require('./routes/users');
+
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메소드
+
+app.use('/user', usersRouter);
+// app.use(express.static(path.join(__dirname, '../react-project/build')));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, '../react-project/build/index.html'))
+// });
+
+
+// app.get('*', ()  => {
+//   res.sendFile(path.join(__dirname, '../react-project/build/index.html'))
+// })
+
+
+module.exports = app;
