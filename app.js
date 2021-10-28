8e895c0db83b5dc5b8a752263076bb5215b85ab1
hyunlee214 <hyun110214@naver.com>
2021-10-28 13:16:07 +0900

    [기능추가]: sequelize로 DB연동기능 추가 및 확인 user파일 관리를 위해 파일구조 소폭 변경하였음.

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 "use strict";
 
-const express = require("express");
-const path = require("path");
-const app = express();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-const PORT = 3000;
-
+const indexRouter = require('./routes/home/index');
 const home = require("./routes/home");
 const usersRouter = require('./routes/users');
+const models = require('./models/index.js');
+
+const app = express();
+
+models.sequelize.sync().then(() => {
+  console.log('db연결성공');
+}). catch(err => {
+  console.log('db연결실패');
+  console.log(err);
+})
 
 
-app.set("views", "./views");
+app.set('views', path.join(__dirname, 'views'))
 app.set("view engine", "ejs");
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메소드
@@ -21,11 +32,24 @@ app.use('/user', usersRouter);
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, '../react-project/build/index.html'))
 // });
-
+app.use(function(req, res, next) {
+  next(createError(404))
+})
 
 // app.get('*', ()  => {
 //   res.sendFile(path.join(__dirname, '../react-project/build/index.html'))
 // })
 
+
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,39 @@
 const express = require('express');
 const router  = express.Router();
 
+// const getUsersCollection = require('')      // DB파일로
 
+function redirectWithMsg({ res, dest, error, info }) {
+  res.redirect(`${dest}?${makeQueryString({ info, error })}`);
+}
+
+// 192.168.0.56:3000/user/signup
+router.get('/signUp', (req, res) => {
+  res.render('user/signup');
+});
+
+router.post('/signIn', async (req, res) => {
+  if (!req.body) {
+    redirectWithMsg({
+      res,
+      dest: '/',
+      error: '잘못된 요청입니다',
+    })
+    return;
+  }
+
+  // DB users
+  const users = await getUsersCollection();
+  const { email, password } = req.body;
+  
+  if (!email || !password) {
+    redirectWithMsg({
+      res,
+      dest: '/',
+      error: '이메일과 비밀번호를 모두 입력해 주세요'
+    })
+  }
+})
 
 
 
